37bac81429454d27be54596fbcd52813282b900f
Joel Marriott <37147533+joelmarriott@users.noreply.github.com>
2022-01-01 20:59:52 +0000

    Add files via upload

--- tictactoe.js
+++ tictactoe.js
@@ -1,106 +1,30 @@
-var squareOne = document.querySelector('#one')
-var squareTwo = document.querySelector('#two')
-var squareThree = document.querySelector('#three')
-var squareFour = document.querySelector('#four')
-var squareFive = document.querySelector('#five')
-var squareSix = document.querySelector('#six')
-var squareSeven = document.querySelector('#seven')
-var squareEight = document.querySelector('#eight')
-var squareNine = document.querySelector('#nine')
+var squares = document.querySelectorAll('td')
 var restart = document.querySelector('#restart')
 
-function makeX(square) {
-  square.textContent = "X";
-  square.style.color = 'blue';
+// Clear playing board
+function clearBoard() {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].textContent = '';
+  }
 }
 
-function makeO(square) {
-  square.textContent = "O";
-  square.style.color = 'red';
+// Restart button
+restart.addEventListener("click",clearBoard)
+
+// Check/Change the square marker
+function changeMarker() {
+  if(this.textContent === '') {
+    this.textContent = 'X';
+    this.style.color = 'blue';
+  }else if (this.textContent === 'X') {
+    this.textContent = 'O';
+    this.style.color = 'red';
+  }else {
+    this.textContent = '';
+  }
 }
 
-// On Click
-restart.addEventListener("click",function(){
-  squareOne.textContent = "";
-  squareTwo.textContent = "";
-  squareThree.textContent = "";
-  squareFour.textContent = "";
-  squareFive.textContent = "";
-  squareSix.textContent = "";
-  squareSeven.textContent = "";
-  squareEight.textContent = "";
-  squareNine.textContent = "";
-})
-
-squareOne.addEventListener("click",function(){
-  makeX(squareOne)
-})
-
-squareTwo.addEventListener("click",function(){
-  makeX(squareTwo)
-})
-
-squareThree.addEventListener("click",function(){
-  makeX(squareThree)
-})
-
-squareFour.addEventListener("click",function(){
-  makeX(squareFour)
-})
-
-squareFive.addEventListener("click",function(){
-  makeX(squareFive)
-})
-
-squareSix.addEventListener("click",function(){
-  makeX(squareSix)
-})
-
-squareSeven.addEventListener("click",function(){
-  makeX(squareSeven)
-})
-
-squareEight.addEventListener("click",function(){
-  makeX(squareEight)
-})
-
-squareNine.addEventListener("click",function(){
-  makeX(squareNine)
-})
-
-// Double Click
-squareOne.addEventListener("dblclick",function(){
-  makeO(squareOne)
-})
-
-squareTwo.addEventListener("dblclick",function(){
-  makeO(squareTwo)
-})
-
-squareThree.addEventListener("dblclick",function(){
-  makeO(squareThree)
-})
-
-squareFour.addEventListener("dblclick",function(){
-  makeO(squareFour)
-})
-
-squareFive.addEventListener("dblclick",function(){
-  makeO(squareFive)
-})
-
-squareSix.addEventListener("dblclick",function(){
-  makeO(squareSix)
-})
-
-squareSeven.addEventListener("dblclick",function(){
-  makeO(squareSeven)
-})
-
-squareEight.addEventListener("dblclick",function(){
-  makeO(squareEight)
-})
-
-squareNine.addEventListener("dblclick",function(){
-  makeO(squareNine)
-})
+// Each square needs to check
+for (var i = 0; i < squares.length; i++) {
+  squares[i].addEventListener('click', changeMarker)
+}
